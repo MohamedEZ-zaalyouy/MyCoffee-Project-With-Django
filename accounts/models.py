f348312fb8490c6a145996fb8a94d1e22dd534a7
MohamedEZ-zaalyouy <mohamedezzaalyouy.98@gmail.com>
2023-03-09 11:39:10 +0100

    models user pro

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    address= models.CharField(max_length=60)
+    address2= models.CharField(max_length=60)
+    city= models.CharField(max_length=60)
+    state= models.CharField(max_length=60)
+    zop_number= models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.user.username
