eb6c0731979b36d1cf9912f6f63c0c05a3fdac19
MohamedEZ-zaalyouy <mohamedezzaalyouy.98@gmail.com>
2023-03-13 23:21:22 +0100

    add cart

--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('add_to_cart',views.add_to_cart, name='add_to_cart'),
+    path('cart',views.cart, name='cart'),
     ]
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,55 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from products.models import Product
+from orders.models import Order, OrderDetail
+from django.utils import timezone
 
 
 # Create your views here.
+##########################################
+# Start Add to cart  Views
+##########################################
+
+@login_required(login_url='signin')
 def add_to_cart(request):
-    return render(request,'orders/order.html')
+    if 'pro_id' in request.GET and 'quantity' in request.GET:
+        pro_id =  request.GET['pro_id']
+        quantity =  request.GET['quantity']
+
+        if not Product.objects.all().filter(id=pro_id).exists():
+            return redirect('products')
+        
+        product = Product.objects.get(id=pro_id)
+        price = product.price
+        order = Order.objects.all().filter(user=request.user, is_finished = False)
+       
+
+        if order:
+            old_order = Order.objects.get(user=request.user, is_finished = False)
+            orderdetail = OrderDetail.objects.create(product = product, order = old_order, price = price, quantity=quantity )
+            orderdetail.save()
+
+            messages.success(request, 'Was added to cart for old order')
+        else:
+            new_order = Order()
+            new_order.user = request.user
+            new_order.is_finished=False
+            new_order.save()
+
+            orderdetail = OrderDetail.objects.create(product = product, order = new_order, price = price, quantity=quantity )
+            orderdetail.save()
+
+            messages.success(request, 'Was added to cart for new order')
+
+        return redirect('/product/'+ request.GET['pro_id'])
+    else:
+        return redirect('products')
+
+
+##########################################
+# Start cart  Views
+##########################################
+
+def cart(request):
+    return render(request,'orders/cart.html')
